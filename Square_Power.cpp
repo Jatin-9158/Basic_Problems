37d0e862a2438b82eb12a64dd42c0b5ac02c1d03
Jatin-9158 <156439117+Jatin-9158@users.noreply.github.com>
2024-01-16 00:08:01 +0530

    Adding_Negative_Power_Cases

--- Square_Power.cpp
+++ Square_Power.cpp
@@ -3,7 +3,32 @@
 using namespace std;
 class Solution {
 public:
-    
+    double myPow(double x, int n) {
+        if(n<0)
+        {
+           n=abs(n);
+           if(n%2==0)
+           {
+            return myPow((1/x)*(1/x),n/2);
+           }
+           else
+           {
+             return (1/x)*myPow((1/x)*(1/x),(n-1)/2);
+           }
+
+        }
+        if(n==0)
+        {
+           return 1;
+        }
+        if(n%2==0)
+        {
+            return myPow(x*x,n/2);
+        }
+        else
+        {
+            return x*myPow(x*x,(n-1)/2);
+        }
     }
 };
 int main()
