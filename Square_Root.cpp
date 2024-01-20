9d95342fbba856db85e540fcc07222ff774962b8
Jatin-9158 <156439117+Jatin-9158@users.noreply.github.com>
2024-01-20 20:15:46 +0530

    Added Precision Version

--- Square_Root.cpp
+++ Square_Root.cpp
@@ -1,37 +1,47 @@
 #include<iostream>
 #include<windows.h>
-int Square_Root(int Number)
+double Square_Root_Unprecised(int x)
 {
-    if(Number==0)
-            return 0;
-    else if (Number==1)
-            return 1;
-    else
+  int mid,sqrt_1;
+  if(x==0 || x==1)
+            return x;
+  else if(x>0)
+  {
+    int low=0;
+    int high=x;
+    while(low<=high)
     {
-        int low,mid,high,sqrt;
-        low=0;
-        high=Number;
-        while(low<=high)
-        {
-            mid=(low+high)/2;
-            if(mid*mid==Number)
-            {
-                sqrt=mid;
-                return sqrt;
-            }
-            else if(mid*mid<Number)
-            {
-                low=mid+1;
-                sqrt=mid;
-            }
-            else 
-            {
-                high=mid-1;
-            }
-
-        }
-        return sqrt;
+       mid=(low+high)/2;
+       if(mid*mid==x)
+              return mid;
+       else if(mid*mid<x)
+       {
+            sqrt_1=mid;
+            low=low+1;
+       }
+       else
+       {
+         high=high-1;
+       }
     }
+  }
+    return sqrt_1;
+}
+double Square_Root(int x)
+{
+    double n=Square_Root_Unprecised(x);
+    double step=0.001;
+    while(n*n<=x)
+    {
+       n=n+step;
+       
+    }
+    return n;
+}
+int main()
+{
+ std::cout<<Square_Root(15);
+ return 0;
 }
 int main()
 {
