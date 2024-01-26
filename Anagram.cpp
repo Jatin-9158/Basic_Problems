067c41f92803521220ec4bc6ef330a0790b065da
Jatin-9158 <156439117+Jatin-9158@users.noreply.github.com>
2024-01-27 00:23:11 +0530

    Update Anagram.cpp

--- Anagram.cpp
+++ Anagram.cpp
@@ -6,6 +6,8 @@ int main ()
  string n1,n2;
  cin>>n1;
  cin>>n2;
+ transform(n1.begin(),n1.end(),n1.begin(), ::tolower);
+ transform(n1.begin(),n1.end(),n2.begin(), ::tolower);
  sort(n1.begin(),n1.end());
  sort(n2.begin(),n2.end());
  if(n1==n2)
