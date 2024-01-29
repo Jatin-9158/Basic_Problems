280dd30dbd1cd8fdcdf2ffa205b877b3d3d7394b
Jatin-9158 <156439117+Jatin-9158@users.noreply.github.com>
2024-01-29 23:18:21 +0530

    Create Sum_of_Max_Min_Element_In_Array_with_O(n)_Complexity.cpp

---
+++ Sum_of_Max_Min_Element_In_Array_with_O(n)_Complexity.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+int sumOfMaxMin(int arr[], int n){
+	int Min_No=INT_MAX;
+	int Max_No=INT_MIN;
+	for(int i=0;i<n;i++)
+	{
+	    if(arr[i]<Min_No)
+		{
+			Min_No=arr[i];
+		}
+		if(arr[i]>Max_No)
+		{
+			Max_No=arr[i];
+		}
+	}
+	int sum=Min_No+Max_No;
+	return sum;
+}
